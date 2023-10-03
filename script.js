3dbb3f366af0a9c3b22f9a623fe6d56d60e0fd1a
Revathi P J <pjrevathi4@gmail.com>
2023-10-03 19:43:22 +0530

    code review

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
+let count = 5;
 const apiKey = 'TsqDrGyLLNfvX5YFjJzslSwbbjgTnlgjqGhGJKy4BsE';
 
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
@@ -23,6 +23,7 @@ function imageLoaded() {
         ready = true;
         loader.hidden = true;
         // console.log('ready=', ready);
+        count=30;
     }
 }
 
